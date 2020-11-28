bbd50787b0e486289bd9f7f1d926b4d6c68ce37c
Truong Nguyen <ntt261298@gmail.com>
2020-11-28 17:48:00 +0700

    Initial project

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+
+// View
+app.use('/', express.static(`${__dirname}/public`));
+
+// Runing server
+const port = 3000
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
