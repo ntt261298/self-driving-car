c0928447f99ea78b9509a0ac2dd91a39e3d9abf3
Quang Akiyama <quanganhcf4@gmail.com>
2020-11-29 15:44:20 +0700

    update fence 25-39

--- public/js/map.js
+++ public/js/map.js
@@ -17,9 +17,41 @@ function building_road() {
         [617, 628, 18, 'road'],
         [705, 658, 18, 'road'],
         [851, 708, 18, 'road'],
+        [1190, 591, -18, 'road'], // 25
+        [1226, 554, 100, 'road'], // 26
+        [1239, 601, 36, 'road'], // 27
+        [1263, 642, 90, 'road'], // 28
+        [1289, 600, -25, 'road'], // 29
+        [680, 337, 104, 'road'], // 30
+        [695, 240, 104, 'road'], // 31
+        [739, 300, 10, 'road'], // 32
+        [953, 386, 12, 'road'], // 33
+        [996, 445, 104, 'road'], // 34
+        [1015, 351, 104, 'road'], // 35
+        [1178, 306, 80, 'road'], // 36
+        [1195, 401, 82, 'road'], // 37
+        [1232, 345, -10, 'road'], // 38
+        [1470, 297, -30, 'road'], // 39
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     ]
-    obstacle_group = game.add.group(); 
-    for (var i = 0; i < road_map.length; i++){
+    obstacle_group = game.add.group();
+    for (var i = 0; i < road_map.length; i++) {
         create_a_fence(road_map[i])
     }
 }
@@ -42,7 +74,7 @@ function creating_goal(x, y) {
     goal = game.add.sprite(x, y, 'goal');
     goal.anchor.set(0.5);
     goal.scale.set(0.3);
-    
+
     game.physics.p2.enable(goal, false);
     goal.body.setCollisionGroup(goalCollisionGroup);
     goal.body.collides(carCollisionGroup);
@@ -52,7 +84,7 @@ function creating_goal(x, y) {
 }
 
 // Fences to make road
-function create_a_fence(road_map){
+function create_a_fence(road_map) {
     var fence = game.add.sprite(road_map[0], road_map[1], road_map[3]);
     fence.anchor.set(0.5);
     fence.scale.set(1);
@@ -60,16 +92,16 @@ function create_a_fence(road_map){
     game.physics.p2.enable(fence, false);
     fence.body.angle += road_map[2];
     fence.body.static = true
-    
+
     fence.body.setCollisionGroup(obstacleCollisionGroup);
     fence.body.collides([carCollisionGroup, obstacleCollisionGroup, signalXCollistionGroup, signalYCollistionGroup, signalFrontCollistionGroup, signalFrontXCollistionGroup, signalFrontYCollistionGroup]);
-    
+
     obstacle_group.add(fence);
 }
 
 // Creating traffic lights -----------
-const greenLight = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19];
-const redLight   = [10,11,12,13,14,15,16,17,18,19,0,1,2,3,4,5,6,7,8,9];
+const greenLight = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+const redLight = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 var trafficLight_position = [
     // d
@@ -99,8 +131,8 @@ var trafficLight_position = [
 
 function building_trafficLight() {
     trafficLight_group = game.add.group();
-    
-    for (var i = 0; i < trafficLight_position.length; i++){
+
+    for (var i = 0; i < trafficLight_position.length; i++) {
         create_trafficLight(trafficLight_position[i])
     }
 }
@@ -110,16 +142,16 @@ function create_trafficLight(light_map) {
     light.anchor.set(0.5);
     light.scale.set(0.5);
 
-    light.animations.add('light',  light_map[3], 2, true); // 1 = 1 frames in a second
+    light.animations.add('light', light_map[3], 2, true); // 1 = 1 frames in a second
     light.animations.play('light');
 
     game.physics.p2.enable(light, false);
     light.body.angle += light_map[2];
     light.body.static = true
-    
+
     light.body.setCollisionGroup(trafficLightCollisionGroup);
     light.body.collides([trafficSignalCollistionGroup]);
-    
+
     obstacle_group.add(light);
 }
 
@@ -130,24 +162,24 @@ var obstacle3;
 var obstacle4;
 
 function creating_obstacles(x, y) {
-   obstacle1 = game.add.sprite(25, 50, 'obstacle');
-   obstacle1.scale.set(0.25);
+    obstacle1 = game.add.sprite(25, 50, 'obstacle');
+    obstacle1.scale.set(0.25);
 
-   obstacle2 = game.add.sprite(75, 50, 'obstacle');
-   obstacle2.scale.set(0.25);
+    obstacle2 = game.add.sprite(75, 50, 'obstacle');
+    obstacle2.scale.set(0.25);
 
-   obstacle3 = game.add.sprite(125, 50, 'obstacle');
-   obstacle3.scale.set(0.25);
-   
-   obstacle4 = game.add.sprite(175, 50, 'obstacle');
-   obstacle4.scale.set(0.25);
-   
-   game.physics.p2.enable([obstacle1, obstacle2, obstacle3, obstacle4], false);
-   
-   set_up_object_collision(obstacle1)
-   set_up_object_collision(obstacle2)
-   set_up_object_collision(obstacle3)
-   set_up_object_collision(obstacle4)
+    obstacle3 = game.add.sprite(125, 50, 'obstacle');
+    obstacle3.scale.set(0.25);
+
+    obstacle4 = game.add.sprite(175, 50, 'obstacle');
+    obstacle4.scale.set(0.25);
+
+    game.physics.p2.enable([obstacle1, obstacle2, obstacle3, obstacle4], false);
+
+    set_up_object_collision(obstacle1)
+    set_up_object_collision(obstacle2)
+    set_up_object_collision(obstacle3)
+    set_up_object_collision(obstacle4)
 }
 
 function set_up_object_collision(object) {
